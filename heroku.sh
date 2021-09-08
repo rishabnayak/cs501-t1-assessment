105131f50930ac1a28dc1747a82c395dd5a68328
Rishab Nayak <rishab@bu.edu>
2021-09-07 17:25:08 -1000

    change launch script

--- heroku.sh
+++ heroku.sh
@@ -3,5 +3,4 @@
 export FLASK_APP=project.server
 export APP_SETTINGS="project.server.config.ProductionConfig"
 flask db init
-flask db migrate
 flask db upgrade
--- launch.sh
+++ launch.sh
@@ -3,7 +3,6 @@
 export FLASK_APP=project.server
 export APP_SETTINGS="project.server.config.DevelopmentConfig"
 flask db init
-flask db migrate
 flask db upgrade
 #flask run --host=0.0.0.0 --port=5000
 flask run
