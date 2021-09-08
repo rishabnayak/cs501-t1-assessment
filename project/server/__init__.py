12e72be0d412556d939ba88955fa834c049c40c9
Rishab Nayak <rishab@bu.edu>
2021-09-07 17:21:13 -1000

    change default env

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -1,3 +1,5 @@
+from project.server.auth.views import auth_blueprint, user_blueprint
+from project.server.models import User
 import os
 import sys
 
@@ -24,27 +26,28 @@ app = Flask(__name__)
 
 app_settings = os.getenv(
     'APP_SETTINGS',
-    'project.server.config.DevelopmentConfig'
+    'project.server.config.ProductionConfig'
 )
 app.config.from_object(app_settings)
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
-from project.server.models import User
 migrate = Migrate(app, db)
+
 
 @app.route("/")
 def root_site():
     return "<p>It works!</p>"
 
-from project.server.auth.views import auth_blueprint, user_blueprint
+
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(user_blueprint)
 
+
 @app.cli.command()
 @click.option('--coverage/--no-coverage', default=False,
-                help='Run tests under code coverage.')
+              help='Run tests under code coverage.')
 def test(coverage):
     """Run the unit tests."""
     if coverage and not os.environ.get('FLASK_COVERAGE'):
